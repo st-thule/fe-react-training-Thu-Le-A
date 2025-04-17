93ba1b23d013094e1e6dd63ce773fea18b8caf7e
st-thule <thu.le@supremetech.vn>
2025-04-17 09:32:39 +0700

    remove node modules

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import '../tailwind.css';
+import { Home } from './pages/home/Home';
 
-function App() {
-  return (
-    <>
-    </>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
